f3b7174b4c8655f0aa02d81c35fe53fe88d8ae28
Jorge681jorge <gutierrez.jorge681@gmail.com>
2022-09-21 14:26:30 -0500

    Voltaje a binario con signo

--- src/app/codification.service.ts
+++ src/app/codification.service.ts
@@ -112,4 +112,72 @@ export class CodificationService{
     }
     return text
   }
+
+  vol_to_binaria(voltage : number, vol_text: string){
+    let binary_text ="";
+
+    let cant_interval = 16;
+    let segments_array = [this.codification.bits]; //8
+    let interval_array = [cant_interval]; //16
+    let tam_interval = voltage / (cant_interval * this.codification.bits) //x, vol = 1
+    segments_array = this.to_segmento(tam_interval);
+    interval_array = this.to_interval(tam_interval);
+    
+    for (const vol of vol_text.split(" ")) {
+      let binario = ""
+      if (vol != "") {
+        if (vol.substring(0, 1) == '+'){ //signo positivo
+          binario += "0";
+        }else{ //signo negativo
+          binario += "1";
+        }
+        let mun_segmento = -1;
+        for (var i = 0; i < (segments_array.length-1); i++) { //buscar segmento
+          //binary_text += i + " s:" +segments_array[i] + " v:" +parseFloat(vol.substring(1)) + " sig:" + segments_array[i+1]+ " "
+          if(segments_array[i]<=parseFloat(vol.substring(1)) && segments_array[i+1]>parseFloat(vol.substring(1))){
+            mun_segmento = i
+              break;
+            }
+        }
+
+        if(mun_segmento == -1){//verifcar que encontro un segmento -> mun_segmenyo 
+          mun_segmento = (segments_array.length-1);
+        }
+        
+        // buscar intervalo
+
+        let mun_intervalo = -1;
+        let aux = parseFloat(vol.substring(1)) - segments_array[mun_segmento];
+        //console.log("nn",aux, parseFloat(vol.substring(1)), segments_array[mun_segmento]);
+        for (var i = 0; i < (interval_array.length-1); i++) { //buscar segmento
+          if(interval_array[i]<=aux && interval_array[i+1]>aux){
+            mun_intervalo = i
+              break;
+            }
+        }
+
+        if(mun_intervalo == -1){//verifcar que encontro un intervalo -> mun_intervalo, sino significa que es el ultimo 
+          mun_intervalo = (interval_array.length);
+        }
+
+        // tocar pasar el mun_segmenyo a binario si es 8bits, entonees solo puete tener 3 ceros -> 1 = 001, 4 = 100
+        let seg_binario = this.pasarabinario(mun_segmento, 3) //pasar a binario
+        let intr_binario = this.pasarabinario(mun_intervalo, 4) //pasar a binario
+
+        binario += seg_binario+""+intr_binario;
+
+        binary_text += binario+" "
+      }
+    }
+
+    return binary_text //+ this.print_array(segments_array);
+  }
+
+  pasarabinario(x: Number, bits: Number){
+    let num = x.toString(2);
+    while(num.length<bits){
+      num ="0"+num
+   }
+    return num
+  }
 }
